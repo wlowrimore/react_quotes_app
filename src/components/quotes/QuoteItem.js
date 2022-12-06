340786023d253ea5cee5a56b542092f346a5e096
William Lowrimore <wlowrimore@gmail.com>
2022-12-06 08:53:38 -0600

    adds wild card route for "notFound" page, and history and prompt hooks to form

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -1,5 +1,7 @@
 import classes from "./QuoteItem.module.css";
 
+import { Link } from "react-router-dom";
+
 const QuoteItem = (props) => {
   return (
     <li className={classes.item}>
@@ -9,7 +11,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className="btn">View Fullscreen</a>
+      <Link to={`/quotes/${props.id}`} className="btn">
+        View Fullscreen
+      </Link>
     </li>
   );
 };
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "William", text: "Learning React is fun!" },
+  { id: "q2", author: "Nikki", text: "Learning React sounds tough!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <section>
-      <h1>Quote Details Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
